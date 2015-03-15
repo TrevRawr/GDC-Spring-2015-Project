cc73bf48705537c91347137f818310ecd842d65d
tclellan <tclellan@sfu.ca>
2015-03-14 21:42:40 -0700

    Created a scene for messing around. Added basic character controller, added basic scoring system, put in a skybox

---
+++ GDC Club Project 2015/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	public float forwardMoveSpeed;
+	public float forwardAcceleration;
+	public int verticalSpeed;
+	public int horizontalSpeed;
+
+	public int maxVerticalDistance;
+	public int maxHorizontalDistance;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate(){
+		Movement ();
+	}
+
+	private void Movement(){
+		//clamp player's movement so they can't move beyond certain x and y coordinates
+		rigidBody.position = new Vector3 (Mathf.Clamp (rigidBody.position.x, -maxHorizontalDistance, maxHorizontalDistance), Mathf.Clamp (rigidBody.position.y, -maxVerticalDistance, maxVerticalDistance), rigidBody.position.z);     
+		
+		float verticalMovement = Input.GetAxis ("Vertical");
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		rigidBody.velocity = new Vector3 (horizontalSpeed * horizontalMovement , verticalSpeed * verticalMovement, forwardMoveSpeed);
+		forwardMoveSpeed += forwardAcceleration;
+	}
+}
---
+++ GDC Club Project 2015/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	private float time = 0;
+	private int score = 0;
+	public UnityEngine.UI.Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		score = (int)time;
+		scoreText.text = "Score: " + score;
+	}
+}
