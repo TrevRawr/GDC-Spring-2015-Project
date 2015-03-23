8859e636655f54ae5f041399e87efe5d112b23d5
Peter <pt.4all@hotmail.com>
2015-03-22 21:40:03 -0700

    Created/updated randomRotatator, which rotates an object when spawned and then destroys it after a set time, and levelGenerator, now spawns the assigned gameobject in a random range of locations. also created a stand-in asteroid as a cube with the randomRotator and rigidbody scripts attached to it.

---
+++ GDC Club Project 2015/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject hazard;//obstacle to be spawned, defined in inspector
+	public Vector3 spawnValues;//location range for which the objects to spawn
+	
+	public int hazardCount;//number of obstacles per wave
+	public float spawnWait;//how long to wait between spawning obstacles
+	public float startWait;//how long until the objects start spawning
+	public float waveWait;//how long it is between 'waves,' which is defined by spawning the number in hazardcount, then incrementing
+	void Start () 
+	{
+		StartCoroutine  (Spawnwaves());//the IEnumurator is a coroutine that generates obstacles. not reliant on update()
+	}
+	
+	// Update is called once per frame
+	IEnumerator Spawnwaves()
+	{ 
+		
+		yield return new WaitForSeconds (startWait);
+		while(true)
+		{	
+			for(int i=0; i<hazardCount; i++)
+			{			
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			
+			yield return new WaitForSeconds (waveWait);
+			
+		}
+	}
+
+
+
+
+}
---
+++ GDC Club Project 2015/Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotater : MonoBehaviour {
+
+	public float tumble;
+	public float lifetime=10.0f;
+	void Start()
+	{
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere*tumble;
+		
+	}
+	void Update()
+	{
+		lifetime-=Time.deltaTime;
+		if(lifetime<0)
+		{
+			Destroy (this.gameObject);
+		}
+	}
+
+}
