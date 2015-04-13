94806c54f4f02b7839fb2598a492358b2a8abbcb
tclellan <tclellan@sfu.ca>
2015-04-13 01:17:58 -0700

    pushing Aary's camera changes

---
+++ GDC Club Project 2015/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player.transform.position);
+	}
+}
